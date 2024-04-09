c969d62016f4dc29861dd7ca7ef45b2eb32b2ab6
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-04-09 21:20:44 +0400

    using caroussel library for home component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -16,5 +16,52 @@ const Home = () => {
         .then((data) => setPopularMovies(data.results));
     }, []);
 
+    return (
+      <>
+        <div className="poster">
+          <Carousel
+            showThumbs={false}
+            autoPlay={true}
+            transitionTime={3}
+            infiniteLoop={true}
+            showStatus={false}
+          >
+            {popularMovies.map((movie) => (
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to={`/movie/${movie.id}`}
+              >
+                <div className="posterImage">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${
+                      movie && movie.backdrop_path
+                    }`}
+                  />
+                </div>
+                <div className="posterImage__overlay">
+                  <div className="posterImage__title">
+                    {movie ? movie.original_title : ""}
+                  </div>
+                  <div className="posterImage__runtime">
+                    {movie ? movie.release_date : ""}
+                    <span className="posterImage__rating">
+                      {movie ? movie.vote_average : ""}
+                      <i className="fas fa-star" />{" "}
+                    </span>
+                  </div>
+                  <div className="posterImage__description">
+                    {movie ? movie.overview : ""}
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </Carousel>
+          <MovieList />
+        </div>
+      </>
+    );
+
 
 }
+
+export default Home;
