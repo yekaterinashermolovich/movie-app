7fd7373de81c47b4595c318c4f4464b6d34a2670
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-04-09 22:26:53 +0400

    html structure for card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 
 
-const Card = () => {
+const Cards = () => {
 
     return (
 
@@ -15,4 +15,4 @@ const Card = () => {
 
 };
 
-export default Card;
+export default Cards;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "./MovieList.css";
-import Card from "../Card/Card";
+import Cards from "../Card/Card";
 import { type } from "@testing-library/user-event/dist/type";
 
 
 const MovieList = () => {
 
-    const [movieList, setMovieList] = useState([]);
+    const [movieList, setMovieList] = useState([]); 
 
-    useEffect(() => {
+  useEffect(() => {
+    getData(); 
+  }, []); 
 
-    }, []);
 
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${
-            type? type:"popular"
-        }? api_key=d1152af9757a55d135e39e8e140015f6=en-US`
-         .then((res) => res.json())
-         .then((data) => data.results())
-        )
-    }
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key= d1152af9757a55d135e39e8e140015f6&language=en-US`
+     
+    )
+      .then((res) => res.json()) 
+      .then((data) => setMovieList(data.results)); 
+  };
 
     return (
 
-        <>
+        <div className="movie__list">
+            <h3 className="list__title">
+                {(type ? type: "popular").toUpperCase()}
+            </h3>
+            <div className="list__cards">
+            {movieList.map((movie) => (
+          <Cards movie={movie} />
+        ))};
+            </div>
+        </div>
 
-        </>
-    )
+        
+    );
 
     
 
