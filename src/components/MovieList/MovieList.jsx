db964918fe7f8a7777e2de067d0aee8c8fdf08a6
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-04-09 22:06:05 +0400

    getting api for MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieList.css";
+import Card from "../Card/Card";
+import { type } from "@testing-library/user-event/dist/type";
 
 
 const MovieList = () => {
+
+    const [movieList, setMovieList] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
+
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${
+            type? type:"popular"
+        }? api_key=d1152af9757a55d135e39e8e140015f6=en-US`
+         .then((res) => res.json())
+         .then((data) => data.results())
+        )
+    }
 
     return (
 
