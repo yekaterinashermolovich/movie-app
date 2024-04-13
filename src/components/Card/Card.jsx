eea978c6e7752c6e3af565c74e665022a33e0596
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-04-13 21:06:10 +0400

    component Cards structure

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "./Card.css";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { Link } from "react-router-dom";
 
 
 
-const Cards = () => {
+const Cards = ({movie}) => {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+
+        },1500);
+    },[]); 
 
     return (
 
         <>
+        {" "}
+        {isLoading ? ( 
+            <div className="cards">
+                {" "}
+                <SkeletonTheme color="#202020" highlightColor="#444">
+                    <Skeleton height={250} duration={2} />
+                </SkeletonTheme>
+            </div>
+        )}
 
         </>
     )
