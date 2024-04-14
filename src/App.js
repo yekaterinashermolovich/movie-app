6f3c8eb7062ce65b5cab905216c2560b4b1cee0a
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-04-15 00:47:32 +0400

    new path in app.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { MovieDetail } from "./pages/MovieDetail/MovieDetail";
+import Header from "./components/header/Header";
 import  Home from "./pages/home/home";
 import MovieList  from "./components/MovieList/MovieList";
 
 
 const App = () => {
   return (
-   <div className='App'>
-     <Router>
-       <Routes>
-         <Route index path = "/" element = {<Home />} />
-         <Route path = "movies/:type" element = {<MovieList />} />
-         <Route path = "movie/:id" element = {<MovieDetail />} />
-       </Routes>
-     </Router>
-   </div>
-   
-
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index path="/" element={<Home />}></Route>
+          {/* маршрут должен быть активирован при совпадении с корневым путем */}
+          <Route path="movie/:id" element={<MovieDetail />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
