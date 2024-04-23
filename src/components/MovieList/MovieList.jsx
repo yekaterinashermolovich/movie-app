8a9f1292c6b7a7dadb52e38692eb7e3532c3bf58
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-04-23 22:13:55 +0400

    handlechange func for year and rating

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,19 +5,23 @@ import Cards from "../Card/Card";
 
 const MovieList = () => {
   const [movieList, setMovieList] = useState([]);
-  const [yearFilter, setYearFilter] = useState([]);
-  const [ratingFilter, setRatingFilter] = useState([]);
+  const [yearFilter, setYearFilter] = useState("");
+  const [ratingFilter, setRatingFilter] = useState("");
+
   const { type } = useParams();
 
   useEffect(() => {
     getData();
-  }, []);
-
-  useEffect(() => {
-    getData();
-  }, [type, setYearFilter, setRatingFilter]);
+  }, [type, yearFilter, ratingFilter]);
 
   const getData = () => {
+    let url;
+    switch(type) {
+      case 'top_rated': url=`https://api.themoviedb.org/3/movie/top_rated?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`;
+      break;
+      case 'top_rated': url=`https://api.themoviedb.org/3/movie/upcoming?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`;
+      default: url=`https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`;
+    }
     fetch(
       `https://api.themoviedb.org/3/movie/${
         type ? type : "popular"
@@ -26,23 +30,53 @@ const MovieList = () => {
       .then((res) => res.json())
       .then((data) => {
         let filteredMovies = data.results;
-        if(yearFilter) {
-          filteredMovies = filteredMovies.filter((movie) => new Date(movie.release_date).getFullYear()===parseInt(setYearFilter)) /*esli cto pomenayem na drugoy filter */
-        };
-        if(ratingFilter) {
-          filteredMovies = filteredMovies.filter((movie) => movie.vote_average >= parseFloat(ratingFilter));
+
+        if (yearFilter) {
+          filteredMovies = filteredMovies.filter(
+            (movie) => new Date(movie.release_date).getFullYear() === parseInt(yearFilter)
+          );
         }
+
+        if (ratingFilter) {
+          filteredMovies = filteredMovies.filter(
+            (movie) => movie.vote_average >= parseFloat(ratingFilter)
+          );
+        }
+
         setMovieList(filteredMovies);
       });
+  };
+
+  const handleYearChange = (event) => {
+    setYearFilter(event.target.value);
+  };
+
+  const handleRatingChange = (event) => {
+    setRatingFilter(event.target.value);
   };
 
   return (
     <div className="movie__list">
       <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+
+      <div className="filters">
+        <label>
+          Year:
+          <input type="text" value={yearFilter} onChange={handleYearChange} />
+        </label>
+
+        <label>
+          Rating:
+          <input type="text" value={ratingFilter} onChange={handleRatingChange} />
+        </label>
+      </div>
+
       <div className="list__cards">
-        {movieList.map((movie) => (
-          <Cards key={movie.id} movie={movie} />
-        ))}
+        {movieList && movieList.length > 0 ? (
+          movieList.map((movie) => <Cards key={movie.id} movie={movie} />)
+        ) : (
+          <p>No movies found</p>
+        )}
       </div>
     </div>
   );
